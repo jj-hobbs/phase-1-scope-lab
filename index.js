2b043fd49c7bb9c0e80e2229b14e464b17d8d527
justin.hobbs <justinhobbs@fastmail.com>
2022-05-20 12:59:03 -0500

    commit tree cfb07b0006f44a12d69558da932852efdc2792b8

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'justin'
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'jim'
+}
